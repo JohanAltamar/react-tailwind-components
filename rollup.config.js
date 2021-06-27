1b7fed2ee4ca6c2971b84b3883841b1b7410db96
Johan Altamar <johan.altamar.dev@gmail.com>
2021-06-27 09:41:27 -0500

    Set rollup

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+export default [
+  {
+    input: './src/index.jsx',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        exports: 'named',
+      }
+    ],
+    plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react'],
+      }),
+      external(),
+      resolve({
+        extensions
+      }),
+      terser(),
+    ]
+  }
+];
