8b2c4c52127f0cd2c9ff9e4d6a5e2e649e45af3b
Johan Altamar <Johan.Altamar.-ND@disney.com>
2021-06-27 20:42:38 -0500

    Set rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,28 +3,34 @@ import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
+import typescript from "rollup-plugin-typescript2";
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: './src/index.jsx',
+    input: './src/index.tsx',
     output: [
       {
-        file: 'dist/index.js',
+        file: packageJson.main,
         format: 'cjs',
+        sourceMap: true
       },
       {
-        file: 'dist/index.es.js',
-        format: 'es',
+        file: packageJson.module,
+        format: 'esm',
         exports: 'named',
+        sourceMap: true,
       }
     ],
     plugins: [
       postcss({
         plugins: [],
         minimize: true,
+        extensions: ['.css'],
       }),
+      typescript({ useTsconfigDeclarationDir: true }),
       babel({
         exclude: 'node_modules/**',
         presets: ['@babel/preset-react'],
