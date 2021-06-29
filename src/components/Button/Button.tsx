7bab7577c7b2a670928f8523953829c5587cd5c5
Johan Altamar <Johan.Altamar.-ND@disney.com>
2021-06-29 14:56:56 -0500

    Remove unuseful comments

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './button.css';
 
 export interface ButtonProps {
@@ -10,9 +9,6 @@ export interface ButtonProps {
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button:React.FC<ButtonProps> = ({ primary=false, backgroundColor, size='medium', label, ...props }) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
@@ -26,27 +22,3 @@ export const Button:React.FC<ButtonProps> = ({ primary=false, backgroundColor, s
     </button>
   );
 };
-
-// Button.propTypes = {
-//   /**
-//    * Is this the principal call to action on the page?
-//    */
-//   primary: PropTypes.bool,
-//   /**
-//    * What background color to use
-//    */
-//   backgroundColor: PropTypes.string,
-//   /**
-//    * How large should the button be?
-//    */
-//   size: PropTypes.oneOf(['small', 'medium', 'large']),
-//   /**
-//    * Button contents
-//    */
-//   label: PropTypes.string.isRequired,
-//   /**
-//    * Optional click handler
-//    */
-//   onClick: PropTypes.func,
-// };
-
